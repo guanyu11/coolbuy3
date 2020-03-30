b0217ede15680d0c470f22ceb2178286964dfc85
LEO <908079329@qq.com>
2020-03-30 19:05:45 +0800

    leo 1

--- src/api/api.js
+++ src/api/api.js
@@ -44,3 +44,55 @@ export const ArticleMainApi=(id)=>{
 export const ArticleAboutApi=(query)=>{
     return http.get(api.article.main,query)
 }
+
+// 分类
+export const getClassifyApi = () => http.get(api.classify.classify, {
+    img_size: 'medium',
+    order_by: '-priority',
+    limit: 100
+})
+// 商品列表
+export const getGoodsListApi = (id) => http.get(api.goodsList.goodsList, {
+    img_size: 'small',
+    order_by: '-priority',
+    items_per_shelf: 10,
+    id__in: id,
+    limit: 10,
+    offset: 0
+})
+
+export const getGoodsNavApi = (categoryId) => http.get(api.goodsList.goodsNav + categoryId, {
+    img_size: 'medium'
+})
+
+// details
+export const getDetailsApi = (id) => http.get(api.details.details + id)
+
+// cart
+
+export const getCartApi = () => http.get(api.cart.cart)
+
+
+// 是否选中
+export const selectApi = (data) => http.post(api.cart.select, data)
+
+// 数量变化
+export const countApi = (data) => http.put(api.cart.cart, data)
+
+// del
+export const delApi = (data) => http.put(api.cart.cart, data)
+
+
+// collect
+export const collectApi = (data) => http.post(api.cart.collection, data)
+
+// addGoods
+export const addGoodsApi = (data) => http.post(api.cart.addGoods, data)
+
+// combainGoods
+export const combainGoodsApi=(data)=>http.put(api.cart.combainGoods,data)
+// search
+export const searchApi=()=>http.get(api.search.search)
+
+// 关键字搜索
+export const keywordApi=(data)=>http.post(api.search.keyword,data,{"Content-Type":"application/x-www-form-urlencoded"})
